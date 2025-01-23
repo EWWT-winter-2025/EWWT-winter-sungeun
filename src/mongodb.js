708cafff42494bf5e86ccd1b9665f5969fb20c16
sungeun <ksekse7147@gamil.com>
2025-01-23 15:54:50 +0900

    test

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,25 +1,24 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://localhost:27017/LoginSignUpTutorial")
-.then(()=>{
+mongoose.connect("mongodb://127.0.0.1:27017/LoginSignUpTutorial")
+.then(() => {
     console.log("connected to mongodb")
 })
-.catch((err)=>{
+.catch((err) => {
     console.log("failed to connect")
 })
 
-const LoginSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+const LoginSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
     }
-}
-)
+})
 
-const collection=new mongoose.model("collection1",LoginSchema)
+const collection = new mongoose.model("collection1", LoginSchema)
 
-module.exports=collection
+module.exports = collection
