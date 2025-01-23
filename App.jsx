50549c87ba8a3267804bcf9215981df697e35596
sung-eun2376015 <ksekse7147@gmail.com>
2025-01-23 16:27:22 +0900

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,16 +1,1 @@
-import './App.css'
-import Header from './components/Header'
-import LoginForm from './components/LoginForm'
 
-function App() {
-
-
-  return (
-    <>
-    <Header className='header'/>
-    <LoginForm/>
-    </>
-  )
-}
-
-export default App
